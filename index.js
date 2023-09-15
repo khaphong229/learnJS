2ee7db2a99e8d0f756dece5ebf61414fc2967f62
Phong Kha IT <khaphong217@gmail.com>
2023-09-15 21:53:29 +0700

    js

---
+++ index.js
@@ -0,0 +1,16 @@
+
+// cách ghi chú thông tin
+
+/**
+ * kasjdkasdjasdlk
+ * ádhasjdhasdjkashdkjash
+ * jhasdjkashdajksdashd
+ * ádhajsdhasldhasdl
+ */
+
+// ctrl /
+
+var fullName = 'Phong Kha';
+var old = 18; 
+alert(fullName);
+alert(old);
